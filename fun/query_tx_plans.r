64afc48bac66ea97d5bca86c4984c4324fb9d1ad
Matt Parker <mattmparker@gmail.com>
2013-04-23 18:24:19 -0600

    Add vars, return latest plan only, only query plans by our staff.

--- fun/query_tx_plans.r
+++ fun/query_tx_plans.r
@@ -7,12 +7,6 @@
 
 
 
-
-# Strings ain't factors
-options(stringsAsFactors = FALSE)
-
-
-
 query_tx_plans <- function(start_date,
                            stop_date = Sys.Date(),
                            odbc = "tbdbplus64") {
@@ -27,38 +21,47 @@ query_tx_plans <- function(start_date,
         "SELECT person_id,
                 treat_plan,
                 treat_plan_type,
+                ltbi_drug,
                 plan_author,
                 author_affiliation,
                 treat_plan_date,
                 plan_status,
                 reason_stopped,
+                completion_status,
                 treat_plan_end,
                 n_tx_completed,
                 days_to_end
          FROM Tx_Plan_View
-         WHERE treat_plan_date BETWEEN #", 
+         WHERE author_affiliation = 'Denver Metro TB Clinic'
+            AND treat_plan_date BETWEEN #", 
             start_date, 
             "# AND #", 
             stop_date, 
             "#",
+            "ORDER BY person_id, treat_plan_date DESC",
         sep = "")
     )
 
     odbcClose(dbconnect)
 
+    
+    # Select the most recent plan for each person
+    latest_plan <- plans[!duplicated(plans$person_id), ]
+
+
 
     # Add month-, quarter-, and year-of-plan variables for ease of aggregation
-    plans$plan_mon <- as.character(
-        format(plans$treat_plan_date, format = "%m")
+    latest_plan$plan_mon <- as.character(
+        format(latest_plan$treat_plan_date, format = "%m")
     )
 
-    plans$plan_yr <- as.character(
-        format(plans$treat_plan_date, format = "%Y")
+    latest_plan$plan_yr <- as.character(
+        format(latest_plan$treat_plan_date, format = "%Y")
     )
 
-    plans$plan_qtr <- (as.numeric(plans$plan_mon) + 2) %/% 3
+    latest_plan$plan_qtr <- (as.numeric(latest_plan$plan_mon) + 2) %/% 3
 
 
-    plans
+    latest_plan
 
 }
